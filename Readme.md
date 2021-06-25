f007316127e404ce8de576b515fc191eea588b21
JonMid <10jonmid@gmail.com>
2021-06-25 08:06:00 -0500

    [add] first commit

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# API RestFul Movies - (Express.js) 🚀
+
+API construida en Node.js y Express.js para trabajar con movies.
+
+## Instalación
+
+Para utilizar este proyecto primero instale las dependencias:
+
+```bash
+npm install
+```
+
+## Uso
+
+```bash
+npm run dev
+```
+
+## Licencia
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const { config } = require('./config/index');
+const moviesApi = require('./routes/movies');
+
+moviesApi(app);
+
+// app.get('/', function (req, res) {
+//   res.send('hello world');
+// });
+
+// app.get('/json', function (req, res) {
+//   res.json({ hello: 'world' });
+// });
+
+app.listen(config.port, function () {
+  console.log(`Listening http://localhost:${config.port}`);
+});
